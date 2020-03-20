dc61b9110b931a593176fff457273dad3e43c6ac
Fhh <546588335@qq.com>
2020-03-20 12:38:36 +0800

    Fhh

--- 1.py
+++ 1.py
@@ -9,11 +9,30 @@
 因为 nums[0] + nums[1] = 2 + 7 = 9
 所以返回 [0, 1]
 """
+
+
 class Solution:
-    def twoSum(self, nums, target) :
+    def twoSum(self, nums, target):
         """
-        :param nums: list[int]
-        :param target: int
-        :return: list[int]
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
         """
-        
+        hashmap = {}
+        for index, num in enumerate(nums):
+            another_num = target - num
+            if another_num in hashmap:
+                return [hashmap[another_num], index]
+            hashmap[num] = index
+        return None
+
+
+class Solution2:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :return: List[int]
+        """
+s=Solution()
+print(s.twoSum(nums = [2, 7, 11, 15], target = 9))
